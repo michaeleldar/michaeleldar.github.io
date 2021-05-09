9cbbe66c95df044665ae4a3dc9239e85e68a24a2
michael <halpinm2010@gmail.com>
2021-05-09 12:26:32 +0100

    djdhdg

--- old/python/t.py
+++ old/python/t.py
@@ -1,11 +1,16 @@
 import random
 from turtle import *
+
 shape("turtle")
 speed(116)
-pencolor("white")
 pensize(6)
+colours = ['blue', 'green', 'yellow', 'red', 'indigo']
 Screen().bgcolor("DarkOrange")
+
+
 def vshape(size):
+    random_num = random.randint(0, 4)
+    pencolor(colours[random_num])
     right(25)
     forward(size)
     backward(size)
@@ -13,20 +18,26 @@ def vshape(size):
     forward(size)
     backward(size)
     right(25)
+
+
 def snow_flake_arm(size):
-    for x in range(0,4):
+    for x in range(0, 4):
         forward(size)
         vshape(size)
-    backward(size*4)
+    backward(size * 4)
+
+
 def snow_flake(size):
-    for x in range(0,18):
+    for x in range(0, 18):
         snow_flake_arm(size)
         right(20)
-for i in range(0,10):
-    size = random.randint(5,30)
-    x = random.randint(-400,400)
-    y = random.randint(-400,400)
+
+
+while True:
+    size = random.randint(5, 30)
+    x = random.randint(-400, 400)
+    y = random.randint(-400, 400)
     penup()
-    goto(x,y)
+    goto(x, y)
     pendown()
     snow_flake(size)
